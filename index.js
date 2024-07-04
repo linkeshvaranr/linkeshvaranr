353a8bdc81db387ce41069a7402ec8fdeaca5639
Linkeshvaran Raja Krishna Kumar <91176949+linkeshvaranr@users.noreply.github.com>
2024-07-04 19:39:53 +0000

    Resume v1

--- index.js
+++ index.js
@@ -1,17 +1,89 @@
 const CFonts = require('cfonts');
 
-function printMessage(message) {
-    CFonts.say(message, {
+
+async function printResume() {
+    const chalkAnimation = await import('chalk-animation');
+    CFonts.say('Linkeshvaran R', {
         font: 'block', 
-        align: 'left',
+        align: 'center',
         colors: ['system'],
         background: 'transparent', 
+        letterSpacing: 1,
+        lineHeight: 0.5,
+        space: true,
+        maxLength: '0',
+    });
+
+    CFonts.say('Contact Info:', {
+        font: 'console',
+        align: 'left',
+        colors: ['system'],
+        background: 'transparent',
         letterSpacing: 1,
         lineHeight: 1,
         space: true,
         maxLength: '0',
     });
+    console.log(`
+    Email: linkeshvaranr@gmail.com
+    Phone: +91 8525076248
+    Website: www.linkeshvaranr.com
+    LinkedIn: linkedin.com/in/linkeshvaranr
+    `);
+
+    CFonts.say('Experience', {
+        font: 'console',
+        align: 'left',
+        colors: ['system'],
+        background: 'transparent',
+        letterSpacing: 1,
+        lineHeight: 1,
+        space: true,
+        maxLength: '0',
+    });
+    console.log(`
+    # Salesforce Developer
+
+      - Developed custom Salesforce solutions.
+      - Built complex console components.
+      - Customized FSL Automations based on Customer needs.
+      - Created Dashboards invloving multiple datasets using SAQL as well as Standard Lens Builder.
+    `);
+
+    CFonts.say('Skills', {
+        font: 'console',
+        align: 'left',
+        colors: ['system'],
+        background: 'transparent',
+        letterSpacing: 1,
+        lineHeight: 1,
+        space: true,
+        maxLength: '0',
+    });
+    console.log(`
+    # Salesforce Development
+
+        - Apex, Visualforce, Lightning
+        - JavaScript, HTML, CSS
+        - REST APIs, SOAP APIs
+
+    # Salesforce FSL 
+
+        - Work Order and Service Appointment Customization / Automation
+        - Gantt 
+
+    # Salesforce Analytics
+
+        - CRM Analytics and Tableau
+        - SQL and SAQL
+    `);
+
+    const animation = chalkAnimation.default.karaoke('I like S O L V I N G problems :)');
+    
+    setTimeout(() => {
+        animation.stop();
+    }, 3000);
+
 }
 
-// Export the function
-module.exports = printMessage;
+module.exports = printResume;
